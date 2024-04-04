a4273a1835863d33be9e606554f3a79d828784b3
nehashashikumar <neha.shashikumar98@gmail.com>
2024-04-04 17:09:19 -0400

    A5 file change

--- Kanbas/Database/index.tsx
+++
@@ -1,9 +0,0 @@
-import courses from "./courses";
-import modules from "./modules";
-import moduleCalendar from "./moduleCalendar"
-import assignments from "./assignments"
-import users from "./users";
-import enrollments from "./enrollments";
-import grades from "./grades";
-
-export {  courses, modules, moduleCalendar, assignments, users, enrollments, grades };
