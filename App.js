88cedf400fdf67e938b4e6790598059f26529115
nehashashikumar <neha.shashikumar98@gmail.com>
2024-04-22 22:54:06 -0400

    a6

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ import "dotenv/config";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 
 mongoose.connect(CONNECTION_STRING);
-//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 const sessionOptions = {
